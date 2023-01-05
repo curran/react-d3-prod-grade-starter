0f7d84cf13205f920bb6ea7965c8e85b161be2f0
Curran Kelleher <68416+curran@users.noreply.github.com>
2023-01-04 20:34:55 -0500

    Remove cruft

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import { Viz } from './Viz';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="App">
       <Viz />
