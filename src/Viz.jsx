ffd1bffda31841e4c6fe1c56b3bf6d715be7cab0
Curran Kelleher <68416+curran@users.noreply.github.com>
2023-01-04 20:34:16 -0500

    White space

--- src/Viz.jsx
+++ src/Viz.jsx
@@ -4,8 +4,10 @@ import { viz } from './viz';
 export const Viz = () => {
   const ref = useRef();
   const [state, setState] = useState({});
+  
   useEffect(() => {
     viz(ref.current, { state, setState });
   }, [state]);
+  
   return <svg ref={ref} />;
 };
